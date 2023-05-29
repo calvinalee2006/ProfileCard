0aef768352f3aa71190eb557ddcda9f1a4cdcd38
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-05-29 10:32:59 -0500

    finished static react project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,42 @@ import "bulma/css/bulma.css";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" imageSrc={AlexaImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard
-        title="Cortana"
-        handle="@cortana32"
-        imageSrc={CortanaImage}
-      />
-
-      <ProfileCard title="Siri" handle="@siri01" imageSrc={SiriImage} />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imageSrc={AlexaImage}
+                description="Alexa was created by amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imageSrc={CortanaImage}
+                description="Cortana was created by Microsoft and is Alexas sister"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                imageSrc={SiriImage}
+                description="Siri was created by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,12 +2,23 @@ function ProfileCard(props) {
   const title = props.title;
   const handle = props.handle;
   const imageSrc = props.imageSrc;
+  const description = props.description;
   return (
-    <>
-      <img src={imageSrc} alt="pda logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
-    </>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={imageSrc} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
   );
 }
 
